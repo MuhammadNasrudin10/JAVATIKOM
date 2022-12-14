efffeb9ae5a6747046548dbe51e4bad3a552f4c6
Muhammad Nasrudin <muhammadnasrudin21@gmail.com>
2022-12-14 20:17:44 +0800

    Membuat js untuk pertanyaan kuis tipe data

---
+++ js/PertanyaanTipeData.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Code yang tidak dieksekusi oleh java ?",
+        answer: "Komentar",
+        options: [
+            "Variabel",
+            "Komentar",
+            "Tipe Data",
+            "Function"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Komentar baris tunggal dapat menggunakan ?",
+        answer: "//",
+        options: [
+            "\\",
+            "+",
+            "/*",
+            "//"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Komentar multi baris dapat menggunakan?",
+        answer: "/*",
+        options: [
+            "\\",
+            "*",
+            "/*",
+            "//"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+        numb: 6,
+        question: "Your Question is Here",
+        answer: "Correct answer of the question is here",
+        options: [
+            "Option 1",
+            "option 2",
+            "option 3",
+            "option 4"
+        ]
+    },
+];
